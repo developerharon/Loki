2d7bf95c70f7ae393b73f4398a1dd749e85d246a
developerharon <haron@ruemerc.co.ke>
2021-09-27 15:54:05 +0300

    initial commit

---
+++ Loki/Components/Rating/LokiRating.razor.cs
@@ -0,0 +1,8 @@
+﻿using Loki.Entities;
+
+namespace Loki.Components.Rating
+{
+    public partial class LokiRating : LokiComponentBase
+    {
+    }
+}
---
+++ Loki/Entities/LokiComponentBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Loki.Entities
+{
+    public class LokiComponentBase : ComponentBase
+    {
+    }
+}
