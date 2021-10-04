33e5ec89deeadaa32e42b842d14f59276f3a0af9
developerharon <haron@ruemerc.co.ke>
2021-10-04 17:11:05 +0300

    added LokiButton with the basic functionality

---
+++ Loki/Components/LokiButton.razor.cs
@@ -0,0 +1,23 @@
+﻿using Loki.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Loki.Components
+{
+    public partial class LokiButton : LokiComponentBase
+    {
+        [Parameter] public string Class { get; set; }
+        [Parameter] public string Style { get; set; }
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        private void OnClickHandler(MouseEventArgs e)
+        {
+            if (Disabled)
+                return;
+
+            OnClick.InvokeAsync(e);
+        }
+    }
+}
