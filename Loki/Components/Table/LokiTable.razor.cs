1ad7a2b244fceaaa3ad281ed9451fe463b3a0ea6
developerharon <haron@ruemerc.co.ke>
2021-10-13 15:17:05 +0300

    I added LokiTable

---
+++ Loki/Components/Table/LokiTable.razor.cs
@@ -0,0 +1,14 @@
+﻿using Loki.Entities;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Loki.Components
+{
+    public partial class LokiTable<T> : LokiComponentBase
+    {
+        [Parameter] public RenderFragment HeaderTemplate { get; set; }
+        [Parameter] public RenderFragment<T> RowTemplate {  get; set; }
+        [Parameter] public RenderFragment FooterTemplate { get; set; }
+        [Parameter] public IEnumerable<T> Data { get; set; }
+    }
+}
