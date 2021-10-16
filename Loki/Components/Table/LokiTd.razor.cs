5c0a6c1e42870197bd47423579a863dce05b33eb
developerharon <haron@ruemerc.co.ke>
2021-10-16 12:50:59 +0300

    added table header and table data components

---
+++ Loki/Components/Table/LokiTd.razor.cs
@@ -0,0 +1,10 @@
+﻿using Loki.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Loki.Components
+{
+    public partial class LokiTd : LokiComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ Loki/Components/Table/LokiTh.razor.cs
@@ -0,0 +1,10 @@
+﻿using Loki.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Loki.Components
+{
+    public partial class LokiTh : LokiComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
